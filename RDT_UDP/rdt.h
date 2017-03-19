29242480a41d88ffc7412837d0e3d71abda1618c
Eman <programmingeman@gmail.com>
2017-03-18 18:40:08 -0700

    Commit correct modified files.

--- RDT_UDP/rdt.h
+++ RDT_UDP/rdt.h
@@ -1,7 +1,38 @@
 #ifndef RDT_H_INCLUDED
 #define RDT_H_INCLUDED
 
+#include <netinet/in.h>
+#include <stdint.h>
+#include <sys/ioctl.h>
 #include <sys/socket.h>
+#include <sys/types.h>
+
+#define PACKET_SIZE 516 // size of the struct packet
+
+int server_port;
+
+// need to fix rdt.c sizes
+struct packet {
+    uint16_t id;
+    uint16_t srcPort;
+    uint16_t dstPort;
+    uint16_t cksum;
+    uint16_t len;
+    uint16_t full_data_sz;
+    uint32_t ackno;
+    uint32_t seqno;
+    char data[500];
+};
+
+typedef struct packet packet_t;
+
+struct pkt_hldr {
+    int id;
+    int full_data_sz;
+    packet_t pkt_list[100]; // needs to be a stack // need to determine a better max pkt list size.
+};
+
+typedef struct pkt_hldr pkt_hldr_t;
 
 int rdt_socket(int address_family, int type, int protocol);
 
